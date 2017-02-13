8702525a9cdc61b100a9d40da852afac1ce76fbd
xxfast <isuru@xfastgames.com>
2017-02-13 09:48:23 +0800

    combined AnimatedSprite with Sprite,
    didn’t made enough sense to specialize the animated behavior

--- graphics/Sprite.js
+++ graphics/Sprite.js
@@ -1,9 +1,7 @@
 class Sprite {
-  /**
+  /** @description
    * Represents a singular sprite with embedded physics.
    * @constructor
-   * @param {string} title - The title of the book.
-   * @param {string} author - The author of the book.
   */
   constructor() {
     this.position = {x:0,y:0};
@@ -13,16 +11,46 @@ class Sprite {
     this.acceleration = {x:0,y:0};
     this.mass = 1;
     this.debug = { drawCollisionMask:0 };
+    this.image = [];
+    this.states = [];
+    this.fpt = 1;
   }
 
-  /*
     * sets layered image resources for the desired sprite
+  /**
+    * returns the current state the sprite is in
+  */
+  state() { return this.states[0]; }
+
+  /**
+    * set the current state of the animation
+    *   @param {string} id - unique name of the animation to go to
+  */
+  goto(state){
+    if(this.states.length==1) return this;
+    for(var i=0; i<this.states.length; i++){
+      if(this.states[i].id==state){
+        this.states.unshift(this.states.splice(i,1));
+        break;
+      }
+    }
+    return this;
+  }
+
+  /**
+  play(fpt=1){
+    this.fpt = fpt;
+    return this;
+  }
+
+  /**
+  /**
     * important ! - the last last layer renders on top
     * important ! - the first layer will be used as the collision mask by defult
-    * @param {string[]} url - location of the images layer by layer.
+    *   @param {string[]} url - location of the images layer by layer.
   */
-  resource(urls){
-    this.image = new Array();
+  source(urls){
+    var state = { id:"default", layers:[] };
     for(var i=0;i<urls.length;i++){
       var image = new Image();
       image.src = urls[i];
@@ -31,13 +59,48 @@ class Sprite {
         if(this.callback.scale.width==0 && this.callback.scale.height==0 )
          this.callback.transform(this.width, this.height);
       }
-      this.image.push(image);
+      state.layers.push(image);
+      this.states.push(state);
       if(!this.collider) this.collider = image;
     }
     return this;
   }
 
-  /*
+  /**
+  * sets animated spritesheet resources for the desired sprite
+  * important ! - the last last layer renders on top
+  * important ! - the first layer will be used as the collision mask by defult
+  *   @param {string} name -  unique name to identify each animation state
+  *   @param {string[]} urls - location(s) of the spritesheet images layer by layer.
+  *   @param {int} nr - number of rows in the spritesheet
+  *   @param {int} nc - number of colomns in the spritesheet
+  *   @param {string} sf -  starting frame of the said state
+  *   @param {int} repeat - times should the animation repeat [-1: repeat forever, 0:don't animate , 1:repeat once ...]
+  */
+  animate(name, urls, nr, nc, nf, sf, repeat){
+    var state = { id:name,frame:sf,layers:[], nr:nr, nc:nc, nf:nf, fw:0,fh:0,repeat:repeat,cp:[]};
+    for(var i=0;i<urls.length;i++){
+      var image = new Image();
+      image.src = urls[i];
+      image.state = state;
+      image.onload = function(){
+        //frame width and height is decided from the base layer
+        if(!this.state.fw) this.state.fw = this.width/this.state.nc;
+        if(!this.state.fh) this.state.fh = this.height/this.state.nr;
+        if(this.state.cp.length==0){
+          for(var r = 0; r < nr; r++)
+          	for(var c = 0; c < nc; c++)
+          		state.cp.push({x:c*this.state.fw ,y:r*this.state.fh});
+        }
+      }
+      state.layers.push(image);
+      if(!this.collider) this.collider = image;
+    }
+    this.states.push(state);
+    return this;
+  }
+
+  /**
     * moves the  sprite to the given position
     * @param {int} x - x position to move.
     * @param {int} y - y position to move.
@@ -47,7 +110,7 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * transform the  sprite to the given scale
     * @param {int} width - desired width.
     * @param {int} height - desired height.
@@ -57,7 +120,7 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * rotates the  sprite by given amount of degrees
     * @param {int} degree - amount of degrees to move.
   */
@@ -66,7 +129,7 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * sets the origin of the sprite to the given position
     * @param {int} x - anchor in the x position.
     * @param {int} x - anchor in the y position.
@@ -76,7 +139,7 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * sets the velocity of the sprite
     * @param {int} x - horizontal velocity component.
     * @param {int} y- vertical velocity component.
@@ -86,18 +149,18 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * increases the acceleration of the sprite
     * @param {int} x - horizontal acceleration component.
     * @param {int} y- vertical acceleration component.
   */
   accelerate(x,y){
     this.acceleration.x += x;
-    this.acceleration.y += x;
+    this.acceleration.y += y;
     return this;
   }
 
-  /*
+  /**
     * applies a force to the sprite
     * @param {int} x - horizontal force component.
     * @param {int} y- vertical force component.
@@ -108,7 +171,7 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * sets the weight of the sprite
     * @param {int} x - weight to set.
   */
@@ -117,7 +180,8 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
+    * @author Joseph Lenton - PlayMyCode.com
     * sets the collision mask of the sprite
     * @param {string} mask - url of the mask to set.
   */
@@ -128,20 +192,22 @@ class Sprite {
     return this;
   }
 
-  /*
+  /**
     * get the collision mask of the sprite
   */
   getCollisionMask(){
     var canvas = document.createElement('canvas');
     var context = canvas.getContext('2d');
     var img = this.collider;
+    // context.save();
+    // context.translate(this.position.x+this.origin.x,this.position.y+this.origin.y);
+    // context.rotate(-this.rotation + Math.PI/2.0);
+    // context.translate(-this.position.x-this.origin.x, -this.position.y-this.origin.y);
     context.drawImage(img, 0, 0, this.scale.width, this.scale.height);
     return context.getImageData(0, 0, this.scale.width,  this.scale.height);
   }
 
   /**
-   * @author Joseph Lenton - PlayMyCode.com
-   *
    * @param {string} sprite - Sprite object this object colliding with.
    */
   colliding(sprite){
@@ -151,11 +217,26 @@ class Sprite {
           x2 = Math.round( sprite.position.x ),
           y2 = Math.round( sprite.position.y );
 
+
       var w  = this.scale.width,
           h  = this.scale.height,
           w2 = sprite.scale.width,
           h2 = sprite.scale.height ;
 
+      var rads = this.rotation * Math.PI/180;
+      var c = Math.abs(Math.cos(rads));
+      var s = Math.abs(Math.sin(rads));
+      w =  h * s + w * c;
+      h = h * c + w * s ;
+      w2 =  h2 * s + w2 * c;
+      h2 = h2 * c + w2 * s ;
+
+      //console.log(w,h,w2,h2);
+
+       w  = this.scale.width,
+          h  = this.scale.height,
+          w2 = sprite.scale.width,
+          h2 = sprite.scale.height ;
       // find the top left and bottom right corners of overlapping area
       var xMin = Math.max( x, x2 ),
           yMin = Math.max( y, y2 ),
@@ -173,7 +254,7 @@ class Sprite {
 
       // get the pixels out from the images
       var pixels  = this.getCollisionMask().data,
-          pixels2 = sprite.getCollisionMask().data;
+      pixels2 = sprite.getCollisionMask().data;
 
       // if the area is really small,
       // then just perform a normal image collision check
@@ -228,13 +309,22 @@ class Sprite {
       return false;
   }
 
-  /*
+  /**
     * updates the sprite once
   */
   update(){
     this.speed = {x: this.speed.x + this.acceleration.x, y: this.speed.y + this.acceleration.y}
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
+    //frame control
+
+    if(this.state().repeat>=1 || this.state().repeat<0){
+      this.state().frame+= this.fpt;
+      if(this.state().frame>this.state().nf-1){
+        this.state().frame = 0;
+        this.state().repeat--;
+      }
+    }
   }
 
   /*
@@ -246,8 +336,9 @@ class Sprite {
     c.translate(this.position.x+this.origin.x,this.position.y+this.origin.y);
     c.rotate(-this.rotation + Math.PI/2.0);
     c.translate(-this.position.x-this.origin.x, -this.position.y-this.origin.y);
-    for(var i=0;i<this.image.length;i++){
-      c.drawImage(this.image[i],this.position.x,this.position.y,this.scale.width,this.scale.height);
+    for(var i=0;i<this.state().layers.length;i++){
+      if(!this.state().hasOwnProperty('frame')) c.drawImage(this.state().layers[i],this.position.x,this.position.y,this.scale.width,this.scale.height);
+      else c.drawImage(this.state().layers[i],this.state().cp[Math.round(this.state().frame)].x, this.state().cp[Math.round(this.state().frame)].y,this.state().fw,this.state().fh,this.position.x,this.position.y,this.scale.width,this.scale.height);
     }
     if(this.debug.drawCollisionMask && this.collider){
       c.drawImage(this.collider,this.position.x,this.position.y,this.scale.width,this.scale.height);
